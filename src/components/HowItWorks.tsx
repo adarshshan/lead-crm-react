8c9f93230dfbcdff63d531a7ec20bfa6e49bf749
adarsh <adarshshanu3@gmail.com>
2025-10-17 10:54:17 +0530

    some responsiveness issues has fixed

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -6,8 +6,8 @@ import arrowimage from "../assets/leadIcons/rightArrow.png";
 
 const HowItWorks = () => {
   return (
-    <div className="bg-[#EAEFFA] py-5 px-4 sm:px-6 md:px-8 lg:px-36 min-h-[60vh] flex flex-col">
-      <div className="w-full flex flex-col justify-start mb-4">
+    <div className="bg-[#EAEFFA] py-9 px-4 sm:px-6 md:px-8 lg:px-36 min-h-[60vh] flex flex-col">
+      <div className="w-full sm:px-6xl flex flex-col justify-start mb-4">
         <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl">
           How It Works
         </h2>
@@ -26,63 +26,63 @@ const HowItWorks = () => {
               Add LeadCRM to Chrome and connect your CRM in 2 minutes—secure and
               seamless.
             </p>
+            <img
+              src="https://img.leadcrm.io/wp-content/uploads/2025/07/08131216/install-the-extension.webp"
+              alt="Install the Extension"
+              className="w-full mt-auto"
+            />
           </div>
-          <img
-            src="https://img.leadcrm.io/wp-content/uploads/2025/07/08131216/install-the-extension.webp"
-            alt=""
-            className="w-full"
-          />
         </div>
         <div className="w-full sm:w-[48%] md:w-[20%] min-h-[30vh] sm:min-h-[35vh] md:min-h-[40vh] shadow-md rounded-2xl p-4 bg-[#ffffff] z-50">
           <div className="w-full flex flex-col justify-center items-center py-4">
             <Two className="size-20" />
             <h3 className="font-semibold text-lg mt-5 text-[#090F4E]">
-              Install the Extension
+              Browse LinkedIn
             </h3>
             <p className="text-[#4C4C4C] text-center text-[.9rem]">
-              Add LeadCRM to Chrome and connect your CRM in 2 minutes—secure and
-              seamless.
+              Search for leads on LinkedIn and let LeadCRM capture key details
+              effortlessly.
             </p>
+            <img
+              src="https://img.leadcrm.io/wp-content/uploads/2025/07/08132934/Browse-LinkedIn-768x533.webp"
+              alt="Browse LinkedIn"
+              className="w-full mt-auto"
+            />
           </div>
-          <img
-            src="https://img.leadcrm.io/wp-content/uploads/2025/07/08132934/Browse-LinkedIn-768x533.webp"
-            alt=""
-            className="w-full"
-          />
         </div>
         <div className="w-full sm:w-[48%] md:w-[20%] min-h-[30vh] sm:min-h-[35vh] md:min-h-[40vh] shadow-md rounded-2xl p-4 bg-[#ffffff] z-50">
           <div className="w-full flex flex-col justify-center items-center py-4">
             <Three className="size-20" />
             <h3 className="font-semibold text-lg mt-5 text-[#090F4E]">
-              Install the Extension
+              Get Enriched Data
             </h3>
             <p className="text-[#4C4C4C] text-center text-[.9rem]">
-              Add LeadCRM to Chrome and connect your CRM in 2 minutes—secure and
-              seamless.
+              Enrich your leads with verified data like emails and company
+              insights instantly.
             </p>
+            <img
+              src="https://img.leadcrm.io/wp-content/uploads/2025/07/08132935/Get-Enriched-Data-768x551.webp"
+              alt="Get Enriched Data"
+              className="w-full mt-auto"
+            />
           </div>
-          <img
-            src="https://img.leadcrm.io/wp-content/uploads/2025/07/08132935/Get-Enriched-Data-768x551.webp"
-            alt=""
-            className="w-full"
-          />
         </div>
         <div className="w-full sm:w-[48%] md:w-[20%] min-h-[30vh] sm:min-h-[35vh] md:min-h-[40vh] shadow-md rounded-2xl p-4 bg-[#ffffff] z-50">
           <div className="w-full flex flex-col justify-center items-center py-4">
             <Four className="size-20" />
             <h3 className="font-semibold text-lg mt-5 text-[#090F4E]">
-              Install the Extension
+              Sync to CRM
             </h3>
             <p className="text-[#4C4C4C] text-center text-[.9rem]">
-              Add LeadCRM to Chrome and connect your CRM in 2 minutes—secure and
-              seamless.
+              Sync enriched leads to your CRM with one click for immediate
+              follow-up.
             </p>
+            <img
+              src="https://img.leadcrm.io/wp-content/uploads/2025/07/08132933/Sync-to-CRM-Instantly-768x418.webp"
+              alt="Sync to CRM"
+              className="w-full mt-auto"
+            />
           </div>
-          <img
-            src="https://img.leadcrm.io/wp-content/uploads/2025/07/08132933/Sync-to-CRM-Instantly-768x418.webp"
-            alt=""
-            className="w-full"
-          />
         </div>
         <div className="absolute w-full flex justify-center mt-6 top-9 left-2">
           <img
@@ -91,6 +91,11 @@ const HowItWorks = () => {
             className="w-full sm:w-[75%] md:w-[60%]"
           />
         </div>
+      </div>
+      <div className="flex justify-center py-6  lg:py-9">
+        <button className="py-3 px-5 bg-[#B3FC6A] rounded-xl text-[#000000] font-semibold sm:mt-9">
+          Try LeadCRM Now
+        </button>
       </div>
     </div>
   );
